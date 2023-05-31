b3d869ad927b01605e2591fceed6dd577da490bd
Walber Costa <walberoc@gmail.com>
2023-05-31 11:44:05 -0300

    inicial commit - funcionando com conexão db com PostGrees

---
+++ src/main/java/com/api/parking_control_nb/ParkingControlNbApplication.java
@@ -0,0 +1,14 @@
+package com.api.parking_control_nb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ParkingControlNbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingControlNbApplication.class, args);
+	}
+
+}
