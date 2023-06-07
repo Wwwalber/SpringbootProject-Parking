c3b7002418597133e896dbed598f5681c108deb8
Walber Costa <walberoc@gmail.com>
2023-06-07 05:29:32 -0300

    Update .gitignore, nbactions.xml, pom.xml, and 2 more files

---
+++ src/test/java/com/api/parking_control_nb/ParkingControlNbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.parking_control_nb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingControlNbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
