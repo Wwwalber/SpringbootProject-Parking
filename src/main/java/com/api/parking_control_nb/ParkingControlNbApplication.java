3d8f90a68b3e5f8c04aaa71f349f4deeb8df4f1e
Walber Costa <walberoc@gmail.com>
2023-06-07 05:28:28 -0300

    testando a chamada da API

--- src/main/java/com/api/parking_control_nb/ParkingControlNbApplication.java
+++ src/main/java/com/api/parking_control_nb/ParkingControlNbApplication.java
@@ -2,13 +2,19 @@ package com.api.parking_control_nb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-
+@RestController //para testar sem criar um source de controler ainda
 public class ParkingControlNbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingControlNbApplication.class, args);
 	}
 
+        @GetMapping("/")
+        public String index(){
+            return "Olá Mundo!";
+        }
 }
